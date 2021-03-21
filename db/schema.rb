7fc3b924c958ee7ee47d5879967a1354c6d23f0e
Areeb Bhaila <areeb.bhaila@gmail.com>
2021-03-21 09:41:53 +0000

    changed data types

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_212735) do
+ActiveRecord::Schema.define(version: 2021_03_21_093340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,12 +79,14 @@ ActiveRecord::Schema.define(version: 2021_03_20_212735) do
     t.string "tender_name"
     t.date "close_date"
     t.text "description"
-    t.integer "volume"
+    t.text "volume"
     t.integer "minimum_bid"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.bigint "drink_id"
+    t.date "start_date"
+    t.datetime "tender_auction_expiry"
     t.index ["drink_id"], name: "index_tenders_on_drink_id"
     t.index ["user_id"], name: "index_tenders_on_user_id"
   end
